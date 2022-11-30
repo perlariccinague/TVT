39dfdea28352a0db4b4b183f1406919704e53662
PN <pn@klapproth-koch.de>
2022-11-30 13:32:34 +0100

    Initial commit

---
+++ templates/header.php
@@ -0,0 +1,33 @@
+<!--<div class="header-bar"></div>
+<div><img src="files/inhalte/bilder/titelbilder/4b.png"></div>
+<div class="header">
+    <div class="header-nav">
+        {{insert_module::1}}
+    </div>
+    <div class="header-bottom">
+        {{insert_content::6}}
+        {{insert_module::3}}
+    </div>
+</div>-->
+
+
+<div class="header-image">
+    <div class="header-bar"></div>
+    <img src="files/inhalte/bilder/titelbilder/4b.png">
+    <div class="header">
+        <div class="header-nav">
+            {{insert_module::1}}
+        </div>
+        <div class="header-bottom">
+            <!--{{insert_content::6}}-->
+            <div class="logo">
+                {{insert_content::9}}
+                {{insert_content::6}}
+            </div>
+
+            {{insert_module::3}}
+        </div>
+    </div>
+</div>
+
+
