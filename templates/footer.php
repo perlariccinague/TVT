0656258f06a4e9eed9676138968a100b5a79a590
PN <pn@klapproth-koch.de>
2022-12-02 15:50:28 +0100

    Vor der Urlaub

---
+++ templates/footer.php
@@ -0,0 +1,14 @@
+<div class="footer">
+    <div class="footer-top">
+        {{insert_content::45     }}
+        {{insert_content::6}}
+    </div>
+    <div class="footer-middle">
+        {{insert_content::46}}
+    </div>
+    <div class="footer-bottom">
+        {{insert_content::47}}
+        {{insert_content::48}}
+        {{insert_content::49}}
+    </div>
+</div>
--- templates/menu.php
+++ templates/menu.php
@@ -1,20 +1,19 @@
-
 <div class="header-image">
     <div class="header-bar"></div>
-    <img src="files/inhalte/bilder/titelbilder/4b.png">
     <div class="header">
-        <div class="header-nav">
-            {{insert_module::1}}
-        </div>
         <div class="header-bottom">
             <div class="logo">
                 {{insert_content::9}}
                 {{insert_content::6}}
             </div>
-
-            {{insert_module::3}}
+            {{insert_module::6}}
         </div>
     </div>
+</div>
+<div class="navigation">
+    <nav class="mod_navigation">
+        {{insert_module::5}}
+    </nav>
 </div>
 
 
